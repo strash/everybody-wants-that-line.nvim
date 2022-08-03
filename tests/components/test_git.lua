2cfe4edacf941e50b629682442e59b1e256d63f7
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2022-08-03 19:48:18 +0300

    fix test components.git

--- tests/components/test_git.lua
+++ tests/components/test_git.lua
@@ -6,6 +6,9 @@ local T = MiniTest.new_set({
 		pre_once = function()
 			child.start({ "-u", "tests/minimal_init.lua" })
 			child.fn.system([[cd dependencies && git init -b "test_branch" && echo "" >> test_file && git add test_file && git commit -m "init"]])
+		end,
+		pre_case = function()
+			child.restart({ "-u", "tests/minimal_init.lua" })
 			child.lua([[require("everybody-wants-that-line").setup()]])
 			child.lua([[M = require("everybody-wants-that-line.components.git")]])
 			child.cmd("cd dependencies | e test_file")
@@ -19,17 +22,19 @@ local T = MiniTest.new_set({
 })
 
 T["cache.branch"] = function()
+	eq(child.fn.isdirectory(".git"), 1)
 	eq(child.lua_get("M.cache.branch"), "test_branch")
 end
 
 T["cache.diff_info"] = function()
-	--child.type_keys("i", "test line", "<Esc>")
-	--child.type_keys("o", "next line", "<Esc>")
-	child.api.nvim_buf_set_text(0, 0, 0, 0, 0, { "text" })
+	eq(child.fn.isdirectory(".git"), 1)
+	child.type_keys("i", "test line", "<Esc>")
+	child.type_keys("o", "next line", "<Esc>")
+	--child.api.nvim_buf_set_text(0, 0, 0, 0, 0, { "text" })
 	child.cmd("w | sleep")
 	---@type git_cache_diffinfo
 	local diff_info = {
-		insertions = 1,
+		insertions = 2,
 		deletions = 1,
 	}
 	eq(child.lua_get("M.cache.diff_info"), diff_info)
